7e20d031c457f725f267e78fafad4ecc9846fc02
Jascha Ehrenreich <jascha@jaeh.at>
2018-06-20 23:15:03 +0200

    remove unused $context var

--- index.php
+++ index.php
@@ -14,9 +14,11 @@
  */
 $context = Timber::get_context();
 $context['posts'] = new Timber\PostQuery();
-$context['foo'] = 'bar';
+
 $templates = array( 'index.twig' );
+
 if ( is_home() ) {
 	array_unshift( $templates, 'home.twig' );
 }
+
 Timber::render( $templates, $context );
